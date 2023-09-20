52b93f03ebc6425dba7e175c5ee3e69987ef66bb
Adam White <madaetihw@gmail.com>
2023-09-20 12:30:32 +0000

    terraform cloud

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ terraform {
       version = "5.17.0"
     }
   }
+  cloud {
+    organization = "RedTurtleSoftware"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 }
 
 provider "random" {
