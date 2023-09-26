4aa98617ab19f9095599255d74c7cf9f1ffd16ea
Adam White <madaetihw@gmail.com>
2023-09-26 10:45:45 +0000

    clean up and move local

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "RedTurtleSoftware"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-        source = "hashicorp/random"
-        version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-  
-}
-
-provider "random" {
-    # Configuration Options  
-}
-
-provider "aws" {
-  # Configuration options
-}
 
 resource "random_string" "bucket_name" {
   length = 16
@@ -36,8 +8,7 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
-
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
