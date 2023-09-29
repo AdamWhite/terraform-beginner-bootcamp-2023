7098d83903e1c0c2689456e0a77f6ae3fee92b0d
Adam White <madaetihw@gmail.com>
2023-09-29 11:41:24 +0000

    CDN bits and foreach for assets

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
     description = "website endpoint for our static website"
     value = module.terrahouse_aws.website_endpoint  
 }
+
+output "cloudfront_url" {
+    description = "the cloudfront distribution domain name"
+    value = module.terrahouse_aws.cloudfront_url
+}
